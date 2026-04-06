b09821833a8cbbce9389ca16faa47b2561d8b8a8
Dilum Randira <dilumrandira16@gmail.com>
2026-04-06 21:57:43 +0530

    create project structure

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
 	try {
+		if (!process.env.MONGO_URI) {
+			console.warn('MONGO_URI is not set; skipping MongoDB connection.');
+			return;
+		}
+
 		const conn = await mongoose.connect(process.env.MONGO_URI);
 
 		console.log(`MongoDB connected: ${conn.connection.host}`);
